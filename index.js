157ec38022969ea3bd69844efd348e94e17f3abd
Pratham <Pratham.singla.762@gmail.com>
2023-06-04 01:22:55 +0530

    connected mongoose to db and setup scripts and styles for layouts

--- index.js
+++ index.js
@@ -2,10 +2,14 @@ const express = require('express');
 const app = express();
 const port = 8000;
 const expressLayouts = require('express-ejs-layouts');  //layout for partials and layouts
-
-//set view engine 
-app.set('view engine','ejs');
+ 
+app.set('view engine','ejs');//set view engine
 app.set('views','./views');
+
+app.set('layout extractStyles',true);//for keeping css file in head
+app.set('layout extractScripts',true); //for keeping js file at bottom of body
+
+app.use(expressLayouts);  // extract style and scripts from sub pages into the layout
 
 
 app.use('/',require('./routes')); //setting up express routes
