9c9ae6622b6ca7794e8085140417586a1badb8a2
Pratham <Pratham.singla.762@gmail.com>
2023-06-05 02:35:06 +0530

    create student profile pagre for updating and deleting student

--- controllers/student_controller.js
+++ controllers/student_controller.js
@@ -2,9 +2,12 @@ const Student = require('../models/student');
 
 
 module.exports.addStudent = (req,res)=>{
+   
+   if(req.isAuthenticated()){
     return res.render('addStudent', {
         title: "SKIT PC | Add Student"
-    })
+    })}
+    return res.redirect("/");
 }
 
 module.exports.create = async(req,res)=>{
@@ -27,4 +30,51 @@ module.exports.create = async(req,res)=>{
         console.log("Invalid User");
         return res.redirect("/users/signup");
     }
+}
+
+module.exports.studentProfile = async(req,res)=>{
+    const student = await Student.findById(req.params.id);
+    try{
+        return res.render('studentProfile',{
+            title:'Student Profile',
+            stud:student
+        });
+    }catch(err){
+        console.log("Error in student profile",err);
+    }
+}
+
+
+
+module.exports.updateStudent = async(req,res)=>{
+    try {
+        const student = await Student.findById(req.params.id);
+          student.name = req.body.name;
+          student.college = req.body.college;
+          student.batch = req.body.batch;
+          student.placementStatus = req.body.placementStatus;
+          student.dsa_score = req.body.dsa_score;
+          student.webdev_score = req.body.webdev_score;
+          student.react_score = req.body.react_score;
+          student.save();
+          return res.redirect("/users/dashboard");
+   
+    } catch (err) {
+        console.log("ERROR IN UPDATING STUDENT",err);
+    }
+
+
+}
+
+module.exports.deleteStudent = async function(req,res){
+    try{
+        let student = await Student.findById(req.params.id);
+        await Student.deleteOne({ _id: req.params.id })
+        console.log("Student deleted");
+          return res.redirect('/users/dashboard');
+        }
+        catch(err){
+        console.log("Errorin deleting student :",err);
+    }
+
 }
--- routes/student.js
+++ routes/student.js
@@ -6,7 +6,12 @@ const passport = require('passport');
 const studentController = require("../controllers/student_controller");
 
 router.get('/addStudent',studentController.addStudent);
-
+router.get('/studentProfile/:id',studentController.studentProfile);
+router.get('/deleteStudent/:id',studentController.deleteStudent);
 router.post('/create',studentController.create);
+
+
+router.post('/updateStudent/:id',studentController.updateStudent);
+
 
 module.exports = router;
